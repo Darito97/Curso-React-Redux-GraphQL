d78a610c5c8e0851f261dd49124a6b4f435af0d3
Darito <davidalonso991@gmail.com>
2022-02-16 14:18:43 -0600

    Agregado de middleware con uso del estado de redux

--- src/Routes.js
+++ src/Routes.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Home from './components/home/HomePage'
 import FavPage from './components/favs/FavPage'
 import LoginPage from './components/login/LoginPage'
 
-function PrivateRoute({ path, component, ...rest }) {
-  let storage = localStorage.getItem('storage')
-  storage = JSON.parse(storage)
-  if (storage && storage.user) {
-    if (storage.user) {
-      return <Route path={path} component={component} />
-    }
+function PrivateRoute({ loggedIn, path, component, ...rest }) {
+
+  if (loggedIn) {
+    return <Route path={path} component={component} />
+
   }
   else {
     return <Redirect to='/login' {...rest} />
   }
 }
 
-export default function Routes() {
+function Routes({ loggedIn }) {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <PrivateRoute path="/favs" component={FavPage} />
+      <PrivateRoute loggedIn={loggedIn} exact path="/" component={Home} />
+      <PrivateRoute loggedIn={loggedIn} path="/favs" component={FavPage} />
       <Route path="/login" component={LoginPage} />
     </Switch>
   )
 }
+function mapStateToProps({ user: { loggedIn } }) {
+  return {
+    loggedIn
+  }
+
+}
+
+export default connect(mapStateToProps)(Routes)
