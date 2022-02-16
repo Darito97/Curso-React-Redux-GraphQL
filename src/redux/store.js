355d0d6775298bc3365833dcf70c82e27d8b995a
Darito <davidalonso991@gmail.com>
2022-02-16 07:07:10 -0600

    Obtencion de datos de caracteres al comienzo de la aplicacion con redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import userReducer from './userDuck'
 import thunk from 'redux-thunk'
+import charsReducer, { getCharactersAction } from './charsDuck'
 
 let rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  characters: charsReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function generateStore() {
   let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+  //Obteniendo los personajes por primera vez al ejecutar la funcion que devuelve getCharactersAction
+  getCharactersAction()(store.dispatch, store.getState)
   return store
 }
