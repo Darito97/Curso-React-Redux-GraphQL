e2018727a56bb5771b02fcad1b28f7324dd6c2f6
Darito <davidalonso991@gmail.com>
2022-02-16 07:24:29 -0600

    Agregado de metodo catch por si hay errores con la obtencion de datos

--- src/redux/charsDuck.js
+++ src/redux/charsDuck.js
@@ -14,19 +14,29 @@ let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR"
 export default function reducer(state = initialData, action) {
   switch (action.type) {
     case GET_CHARACTERS:
+      return { ...state, fetching: true }
     case GET_CHARACTERS_ERROR:
+      return { ...state, fetch: false, error: action.payload }
     case GET_CHARACTERS_SUCESS:
-      return { ...state, array: action.payload }
+      return { ...state, array: action.payload, fetching: false }
     default: return state
   }
 }
 // actions
 export const getCharactersAction = () => (dispach, getState) => {
-
+  dispach({
+    type: GET_CHARACTERS,
+  })
   return axios.get(URL).then(res => {
     dispach({
       type: GET_CHARACTERS_SUCESS,
       payload: res.data.results
+    })
+  }).catch((error) => {
+    console.log(error)
+    dispach({
+      type: GET_CHARACTERS_ERROR,
+      payload: error.response.message
     })
   })
 }
