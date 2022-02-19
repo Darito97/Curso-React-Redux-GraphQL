7b362542f523584a6c897ccaf01273aff796b78a
Darito <davidalonso991@gmail.com>
2022-02-19 12:48:53 -0600

    Agregado de estado para manejar cambios rapidos sin graphql

--- src/components/home/GraphHome.js
+++ src/components/home/GraphHome.js
@@ -4,6 +4,7 @@ import { gql } from 'apollo-boost'
 import { useQuery } from 'react-apollo'
 
 export default function GraphHome() {
+  const [chars, setChars] = useState([])
   let query = gql`
     {
       characters{
@@ -15,12 +16,28 @@ export default function GraphHome() {
     }
   `
   let { data, loading, error } = useQuery(query)
-  if (loading) return <h2>Cargando...</h2>
+
+  useEffect(() => {
+    if (data && !loading && !error) {
+      setChars(data.characters.results)
+    }
+
+  }, [data, loading, error])
+
+  function nextCharacter() {
+    let newChars = [...chars]
+    newChars.shift()
+    setChars([...newChars])
+  }
+
+  if (loading) { return <h2>Cargando...</h2> }
+
+
   return (
     <Card
       //rightClick={addFav} 
-      //leftClick={nextCharacter} 
-      {...data.characters.results[0]}
+      leftClick={nextCharacter}
+      {...chars[0]}
     />
 
   )
